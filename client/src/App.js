42e9c845edef7c899adaa7f5717643fbb4c2ed84
unknown <Sophea.Wiesenfarth-Chan@gmx.de>
2025-02-14 12:26:22 +0100

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import CustomNavbar from "./components/navbar";
 import Home from "./components/home";
 import Projects from "./components/projects";
