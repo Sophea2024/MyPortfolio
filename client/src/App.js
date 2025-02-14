34f1f2605b7c149f7b45da51c19e2950f23c4c8b
unknown <Sophea.Wiesenfarth-Chan@gmx.de>
2025-02-14 12:36:54 +0100

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import CustomNavbar from "./components/navbar";
 import Home from "./components/home";
@@ -6,19 +6,17 @@ import Projects from "./components/projects";
 import Workexperience from "./components/workexperience";
 import "./styles/navbar.css";
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <CustomNavbar />
-        <Routes>
+const App = () => {
+  return (
+    <Router>
+      <CustomNavbar />
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/workexperience" element={<Workexperience />} />
-        </Routes>
-      </Router>
-    );
-  }
-}
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
